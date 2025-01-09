e59cffb455f7fd1214622f3832b6dd2a362dc036
suk.bear <suk.bear.suwu@gmail.com>
2025-01-09 16:35:45 +0800

    feat: configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind'
+import vue from '@astrojs/vue'
+
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    vue({
+      appEntrypoint: '/src/pages/_app',
+      jsx: true,
+      devtools: true,
+    }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+})
